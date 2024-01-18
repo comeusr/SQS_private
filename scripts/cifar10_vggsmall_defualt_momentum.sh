6d8fe8f4e508e72b1aa38d94b04e3eda6a0d4ce4
Ziyi <wang4538@purdue.edu>
2024-01-18 13:20:11 -0500

    initial commit

---
+++ scripts/cifar10_vggsmall_defualt_momentum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+DATASET="--train-dir Path2DatasetCIFAR10/train/ --val-dir Path2DatasetCIFAR10/val/ -d cifar10 --num-classes 10"
+GENERAL="--lr 2e-5 --batch-size 128 --epochs 350 --workers 4 --base-size 32 --crop-size 32"
+INFO="--checkname vggsmall2bit --lr-scheduler one-cycle"
+MODEL="--network vggsmall --mask --K 4 --weight-decay 5e-4 --empirical True"
+PARAMS="--tau 0.01"
+RESUME="--show-info"
+GPU="--gpu-ids 0"
+
+sbatch --time=4:00:00 --nodes=1 --gpus-per-node=1 <<EOT
+#!/bin/bash -l
+
+#sbatch --out /home/wang4538/DGMS-master/out/%j.out
+#sbatch --error /home/wang4538/DGMS-master/out/%j.out
+
+python ../main.py $DATASET $GENERAL $MODEL $INFO $PARAMS $RESUME $GPU
+
+EOT
+
+
