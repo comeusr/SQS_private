0ee05e42d72bb86b082aa2fa5cee02460c8f6de9
Ziyi <wang4538@purdue.edu>
2024-01-22 00:37:20 -0500

    Update ResNet18 quantization train scripts

--- scripts/cifar10_resnet18_defualt_momentum.sh
+++ scripts/cifar10_resnet18_defualt_momentum.sh
@@ -5,7 +5,7 @@ GENERAL="--lr 2e-5 --batch-size 128 --epochs 350 --workers 4 --base-size 32 --cr
 INFO="--checkname resnet182bit --lr-scheduler one-cycle"
 MODEL="--network resnet18 --mask --K 4 --weight-decay 5e-4 --empirical True"
 PARAMS="--tau 0.01"
-RESUME="--resume /scratch/gilbreth/wang4538/DGMS/run/cifar10/resnet18_32bit_uncompressed/experiment_0/checkpoint.pth.tar --rt --show-info"
+RESUME="--rt --show-info"
 GPU="--gpu-ids 0"
 
 sbatch --time=4:00:00 --nodes=1 --gpus-per-node=1 <<EOT
