3df25836c5237c977639a904caf97b0491629a9f
Ziyi Wang <wang4538@gilbreth-fe03.rcac.purdue.edu>
2024-01-23 20:21:30 -0500

    Update

--- scripts/cifar10_vggsmall_pretrain.sh
+++ scripts/cifar10_vggsmall_pretrain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 DATASET="--train-dir /home/wang4538/DGMS-master/CIFAR10/train/ --val-dir /home/wang4538/DGMS-master/CIFAR10/val/ -d cifar10 --num-classes 10"
-GENERAL="--lr 0.05 --batch-size 128 --epochs 350 --workers 4 --base-size 32 --crop-size 32 --nesterov"
+GENERAL="--lr 0.05 --batch-size 64 --epochs 350 --workers 4 --base-size 32 --crop-size 32 --nesterov"
 INFO="--checkname vggsmall_32bit --lr-scheduler one-cycle"
 MODEL="--network vggsmall --mask --K 4 --weight-decay 5e-4 --empirical True"
 PARAMS="--tau 0.01"
