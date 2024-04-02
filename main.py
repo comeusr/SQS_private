075d0d924d95b7c90e54f41f81e8f6713b63ae8f
Ziyi <ziyi@Ziyi.local>
2024-04-02 10:52:30 -0700

    typo

--- main.py
+++ main.py
@@ -343,7 +343,7 @@ def main():
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters()) / 1000000.0))
 
     for name, p in model.named_parameters():
-        print(name, p.size)
+        print(name, p.size())
 
     cfg.IS_NORMAL = True if (args.resume is not None) else False
     cfg.IS_NORMAL = args.normal
