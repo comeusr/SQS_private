ed1652cc62189c076a93be2accae36593788c50a
Ziyi <ziyi@Ziyi.lan1>
2024-04-03 01:18:15 -0700

    composer

--- utils/watch.py
+++ utils/watch.py
@@ -4,24 +4,27 @@ from modeling.DGMS import DGMSConv
 import torch.nn as nn
 import torch
 
+
 def check_total_zero(x):
     with torch.no_grad():
         return x.eq(0.0).float().sum().item()
 
+
 def check_total_weights(x):
     with torch.no_grad():
         return x.numel()
+
 
 class Sparsity(Callback):
 
     def __init__(self):
         super().__init__()
 
-    def log_mu_sparsity(self, state:State, event:Event, logger:Logger):
+    def log_mu_sparsity(self, state:State, logger:Logger):
         for name, m in state.model.named_modules():
+            print(name)
             if isinstance(m, DGMSConv):
-                print("Find DGMSConv")
-                wandb.log({name+"mu": wandb.Histogram(m.sub_distribution.mu.detach().data.cpu().numpy())})
+                wandb.log({name+"mu": wandb.Histogram(m.sub_distribution.mu)})
             elif isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
                 total_zero = check_total_zero(m.weight)
                 total_weight = check_total_weights(m.weight)
