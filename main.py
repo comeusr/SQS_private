b791ff79a0b9fd765dac2c56ea4bf082b5c1209a
Ziyi <ziyi@Ziyi.lan1>
2024-04-03 00:10:40 -0700

    composer

--- main.py
+++ main.py
@@ -18,13 +18,7 @@ from mypath import Path
 from dataloader import make_data_loader
 from modeling import DGMSNet
 from modeling.DGMS import DGMSConv
-from utils.sparsity import SparsityMeasure
-from utils.lr_scheduler import get_scheduler
 from utils.PyTransformer.transformers.torchTransformer import TorchTransformer
-from utils.summaries import TensorboardSummary
-from utils.metrics import Evaluator
-from utils.saver import Saver
-from utils.misc import AverageMeter, get_optimizer, resume_ckpt, check_cuda_memory
 from utils.loss import *
 from utils.watch import Sparsity
 
@@ -34,186 +28,7 @@ from composer.optim import DecoupledAdamW, LinearWithWarmupScheduler
 from composer.callbacks import LRMonitor, OptimizerMonitor, NaNMonitor
 
 
-# class Trainer(object):
-#     def __init__(self, args):
-#         self.args = args
-#         cfg.set_config(args)
-#
-#         self.saver = Saver(args)
-#         self.saver.save_experiment_config()
-#
-#         self.summary = TensorboardSummary(self.saver.experiment_dir)
-#         self.writer = self.summary.create_summary()
-#
-#         kwargs = {'num_workers': args.workers, 'pin_memory': True}
-#         self.train_loader, self.val_loader, self.test_loader, self.nclass = make_data_loader(args, **kwargs)
-#
-#         model = DGMSNet(args, args.freeze_bn)
-#
-#         if args.mask:
-#             print("DGMS Conv!")
-#             _transformer = TorchTransformer()
-#             _transformer.register(nn.Conv2d, DGMSConv)
-#             model = _transformer.trans_layers(model)
-#         else:
-#             print("Normal Conv!")
-#
-#         print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
-#
-#         cfg.IS_NORMAL = True if (args.resume is not None) else False
-#         optimizer = get_optimizer(model, args)
-#         cfg.IS_NORMAL = self.args.normal
-#         self.model, self.optimizer = model, optimizer
-#
-#         self.criterion = nn.CrossEntropyLoss()
-#         self.sparsity = SparsityMeasure(args)
-#
-#         self.lr_scheduler = get_scheduler(args, self.optimizer, args.lr, len(self.train_loader))
-#
-#         self.evaluator = Evaluator(self.nclass, self.args)
-#
-#         if args.cuda:
-#             torch.backends.cudnn.benchmark=True
-#             self.model = torch.nn.parallel.DataParallel(self.model, device_ids=self.args.gpu_ids)
-#             self.model = self.model.cuda()
-#
-#         self.best_top1 = 0.0
-#         self.best_top5 = 0.0
-#         self.best_sparse_ratio = 0.0
-#         self.this_sparsity = 0.0
-#         self.best_params = 0.0
-#         if args.resume is not None:
-#             self.model, self.optimizer, self.lr_scheduler, self.best_top1 = \
-#                 resume_ckpt(args, self.model, self.train_loader, self.optimizer, self.lr_scheduler)
-#
-#         print('    Total params (+GMM) : %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
-#
-#         if args.rt:
-#             args.start_epoch = 0
-#
-#     def training(self, epoch):
-#         cfg.set_status(True)
-#         self.model.train()
-#
-#         batch_time = AverageMeter()
-#         data_time = AverageMeter()
-#         losses = AverageMeter()
-#         top1 = AverageMeter()
-#         top5 = AverageMeter()
-#         end = time.time()
-#
-#         train_loss = 0.0
-#         num_img_tr = len(self.train_loader)
-#
-#         tbar = tqdm(self.train_loader)
-#         for i, (image, target) in enumerate(tbar):
-#             data_time.update(time.time() - end)
-#             if self.args.cuda:
-#                 image, target = image.cuda(), target.cuda()
-#             outputs = self.model(image)
-#             loss = self.criterion(outputs, target)
-#
-#             prec1, prec5 = self.evaluator.Accuracy(outputs.data, target.data, topk=(1, 5))
-#             losses.update(loss.item(), image.size(0))
-#             top1.update(prec1.item(), image.size(0))
-#             top5.update(prec5.item(), image.size(0))
-#
-#             self.optimizer.zero_grad()
-#
-#             loss.backward(retain_graph=True)
-#             self.optimizer.step()
-#             self.lr_scheduler.step()
-#             batch_time.update(time.time() - end)
-#             end = time.time()
-#
-#             train_loss = (loss.item() + train_loss)
-#             tbar.set_description('Train Loss: {loss:.4f} | T1: {top1: .3f} | T5: {top5: .2f} | best T1: {pre_best:.2f} T5: {best_top5:.2f} NZ: {nz_val:.4f} #Params: {params:.2f}M | lr: {_lr:.8f}'
-#                 .format(loss=losses.avg,
-#                         top1=top1.avg,
-#                         top5=top5.avg,
-#                         pre_best=self.best_top1,
-#                         best_top5=self.best_top5,
-#                         nz_val=1-self.best_sparse_ratio,
-#                         params=self.best_params,
-#                         _lr=self.optimizer.param_groups[0]['lr']
-#                         ))
-#             self.writer.add_scalar('train/train_loss_iter', loss.item(), i + num_img_tr * epoch)
-#             self.writer.add_scalar('train/top1', top1.avg, i + num_img_tr * epoch)
-#             self.writer.add_scalar('train/top5', top5.avg, i + num_img_tr * epoch)
-#             self.writer.add_scalar('train/lr', self.optimizer.param_groups[0]['lr'], i + num_img_tr * epoch)
-#
-#         self.writer.add_scalar('train/total_loss_epoch', train_loss, epoch)
-#         print('[Epoch: %d, numImages: %5d]' % (epoch, i * self.args.batch_size + image.data.shape[0]))
-#         print('Loss: %.3f' % train_loss)
-#
-#         return (losses.avg, top1.avg)
-#
-#     def validation(self, epoch):
-#         cfg.set_status(False)
-#         num_img_tr = len(self.val_loader)
-#
-#         batch_time = AverageMeter()
-#         data_time = AverageMeter()
-#         losses = AverageMeter()
-#         top1 = AverageMeter()
-#         top5 = AverageMeter()
-#
-#         self.model.eval()
-#         self.evaluator.reset()
-#         tbar = tqdm(self.val_loader, desc='\r')
-#         test_loss = 0.0
-#         end = time.time()
-#         for i, (image, target) in enumerate(tbar):
-#             data_time.update(time.time() - end)
-#
-#             if self.args.cuda:
-#                 image, target = image.cuda(), target.cuda()
-#             with torch.no_grad():
-#                 outputs = self.model(image)
-#             loss = self.criterion(outputs, target)
-#             test_loss += loss.item()
-#
-#             prec1, prec5 = self.evaluator.Accuracy(outputs.data, target.data, topk=(1, 5))
-#             losses.update(loss.item(), image.size(0))
-#             top1.update(prec1.item(), image.size(0))
-#             top5.update(prec5.item(), image.size(0))
-#
-#             batch_time.update(time.time() - end)
-#             end = time.time()
-#
-#             tbar.set_description('({batch}/{size}) Test Loss: {loss:.4f} | Top1: {top1: .4f} | Top5: {top5: .4f}'
-#                 .format(batch=i + 1,
-#                         size=len(self.val_loader),
-#                         loss=losses.avg,
-#                         top1=top1.avg,
-#                         top5=top5.avg,
-#                         ))
-#             self.writer.add_scalar('val/val_loss_iter', loss.item(), i + num_img_tr * epoch)
-#             self.writer.add_scalar('val/top1', top1.avg, i + num_img_tr * epoch)
-#             self.writer.add_scalar('val/top5', top5.avg, i + num_img_tr * epoch)
-#         if self.args.show_info:
-#             self.this_sparsity, this_params = self.sparsity.check_sparsity_per_layer(self.model)
-#             self.writer.add_scalar('val/total_sparsity', self.this_sparsity, epoch)
-#         new_pred = top1.avg
-#         if new_pred > self.best_top1 and not self.args.only_inference:
-#             is_best = True
-#             self.best_top1 = new_pred
-#             self.best_params = this_params
-#             self.best_top5 = top5.avg
-#             self.best_sparse_ratio = self.this_sparsity
-#             bitwidth = math.floor(math.log(cfg.K_LEVEL, 2))
-#             self.saver.save_checkpoint({
-#                 'epoch': epoch + 1,
-#                 'state_dict': self.model.module.state_dict(),
-#                 'optimizer': self.optimizer.state_dict(),
-#                 'best_top1': self.best_top1,
-#                 'best_top5': self.best_top5,
-#                 'params': self.best_params,
-#                 'bits': bitwidth,
-#                 'CR': 1/((1-self.best_sparse_ratio) * bitwidth / 32),
-#                 'according_sparsity': self.this_sparsity,
-#             }, is_best)
-#         return (losses.avg, top1.avg)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Differentiable Gaussian Mixture Weight Sharing (DGMS)",
@@ -327,7 +142,7 @@ def main():
         "--empirical", "True", "--tau", "0.01",
         "--resume", r"..\DGMS\run\cifar10\vggsmall_32bit_uncompressed\experiment_9\checkpoint.pth.tar",
         "--rt", "--show-info", "--gpu-ids", "0", "--wandb_watch", "--t_warmup", "0.1dur", "--alpha_f", "0.001",
-        "--duration", "1ep", "--save_folder", "/scratch/gilbreth/wang4538/DGMS/debug/cifar10", "--autoresume", '--run_name', 'debug'
+        "--duration", "3ep", "--save_folder", "/scratch/gilbreth/wang4538/DGMS/debug/cifar10", "--autoresume", '--run_name', 'debug'
     ])
     # args = parser.parse_args()
     args.cuda = not args.no_cuda and torch.cuda.is_available()
