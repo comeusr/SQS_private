86349182168ce7af6dca665c4656752f675654e1
Ziyi <ziyi@Ziyi.local>
2024-04-03 11:15:10 -0700

    composer

--- scripts/composer_test.sh
+++ scripts/composer_test.sh
@@ -20,7 +20,7 @@ sbatch --time=4:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #SBATCH --error /home/wang4538/DGMS-master/out/%j.out
 
 nvidia-smi
-python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset= ${DATA_NAME} --weight_decay ${WD} \
+python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --t_warmup "0.1dur" --alpha_f ${FINAL_LR} \
        --run_name K${K}_temp${TEMP}_LR${LR} --autoresume --eval_interval ${EVAL_INTERV} \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}
