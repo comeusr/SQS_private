2c526f0a9fdb566b726e9aeabc67a2440f0185d8
Ziyi <ziyi@Ziyi.lan1>
2024-04-03 00:57:53 -0700

    composer

--- utils/watch.py
+++ utils/watch.py
@@ -20,7 +20,8 @@ class Sparsity(Callback):
     def log_mu_sparsity(self, state:State, event:Event, logger:Logger):
         for name, m in state.model.named_modules():
             if isinstance(m, DGMSConv):
-                wandb.log({name+"mu": wandb.Histogram(m.sub_distribution.mu.detach().data)}, commit=False)
+                print("Find DGMSConv")
+                wandb.log({name+"mu": wandb.Histogram(m.sub_distribution.mu.detach().data.cpu().numpy())})
             elif isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
                 total_zero = check_total_zero(m.weight)
                 total_weight = check_total_weights(m.weight)
