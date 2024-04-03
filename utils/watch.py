6fa23dca8e6b65e99b1410684dcfd1fe7bf768bc
Ziyi <ziyi@Ziyi.local>
2024-04-03 10:15:19 -0700

    composer

--- utils/watch.py
+++ utils/watch.py
@@ -3,6 +3,7 @@ from composer import Callback, State, Logger, Event
 from modeling.DGMS import DGMSConv
 import torch.nn as nn
 import torch
+import numpy as np
 
 
 def check_total_zero(x):
@@ -39,8 +40,11 @@ class EpochMonitor(Callback):
             for name, m in state.model.named_modules():
                 if isinstance(m, DGMSConv):
                     print("Found DGMSConv")
-                    wandb.log({name+"mu": wandb.Histogram(m.sub_distribution.mu.detach().data.cpu())})
+                    data = m.sub_distribution.mu.detach().data.cpu().numpy()
+                    hist = np.histogram(data)
+                    wandb.log({name+"mu": wandb.Histogram(np_histogram=hist)})
                 elif isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+                    print("Found Non-DGMS Layer")
                     total_zero = check_total_zero(m.weight)
                     total_weight = check_total_weights(m.weight)
                     wandb.log({name+"sparsity": total_zero/total_weight})
