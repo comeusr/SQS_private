185583d74e4ee88f7f73ecba9647d6889c38b67f
Ziyi <ziyi@MacBook-Pro.local>
2024-04-08 00:42:07 -0400

    max

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -32,16 +32,16 @@ class DGMSNet(ComposerModel):
         inputs, _ = batch
         return self.network(inputs)
 
-    def eval_forward(self, batch, outputs=None):
-        cfg.IS_TRAIN = False
-        if outputs != None:
-            return outputs
-        inputs, _ = batch
-        return self.network(inputs)
-
-    def update_metric(self, batch, outputs, metric):
-        _, targets = batch
-        metric.update(outputs, targets)
+    # def eval_forward(self, batch, outputs=None):
+    #     cfg.IS_TRAIN = False
+    #     if outputs != None:
+    #         return outputs
+    #     inputs, _ = batch
+    #     return self.network(inputs)
+    #
+    # def update_metric(self, batch, outputs, metric):
+    #     _, targets = batch
+    #     metric.update(outputs, targets)
 
     # def get_metrics(self, is_train=False):
     #     # defines which metrics to use in each phase of training
