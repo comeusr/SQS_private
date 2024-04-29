c07ca0b8c5a7e4357cdba2a706a050d8255d4601
Ziyi Wang <wang4538@purdue.edu>
2024-04-29 15:22:21 -0400

    Sparsity

--- modeling/DGMS/GMM.py
+++ modeling/DGMS/GMM.py
@@ -68,7 +68,8 @@ class GaussianMixtureModel(nn.Module):
             initial_region_saliency, pi_init, sigma_init = cluster_weights(init_weights, self.num_components)
         elif method == 'empirical':
             initial_region_saliency, pi_init, sigma_init = cluster_weights(init_weights, self.num_components)
-            sigma_init, _sigma_zero = torch.ones_like(sigma_init).mul(0.01).to(DEVICE), torch.ones_like(torch.tensor([_sigma_zero])).mul(0.01).to(DEVICE)
+            sigma_init = torch.ones_like(sigma_init).mul(0.01).to(DEVICE),
+            # sigma_init, _sigma_zero = torch.ones_like(sigma_init).mul(0.01).to(DEVICE), torch.ones_like(torch.tensor([_sigma_zero])).mul(0.01).to(DEVICE)
         self.mu = nn.Parameter(data=torch.mul(self.mu.to(DEVICE), initial_region_saliency.flatten().to(DEVICE)))
         self.pi_k = nn.Parameter(data=torch.mul(self.pi_k.to(DEVICE), pi_init)).to(DEVICE).float()
         # self.pi_zero = nn.Parameter(data=torch.tensor([pi_zero_init], device=self.device)).to(DEVICE).float()
