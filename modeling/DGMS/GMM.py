8df4dd6ec02a3ee48a54a13c8f70c7e3246c7bd1
Ziyi Wang <wang4538@purdue.edu>
2024-04-30 21:55:26 -0400

    sparsity

--- modeling/DGMS/GMM.py
+++ modeling/DGMS/GMM.py
@@ -32,8 +32,7 @@ class GaussianMixtureModel(nn.Module):
         self.params_initialization(init_weights, init_method)
         self.prune = cfg.PRUNE
         self.mask = (init_weights.abs()< 0.0).to(DEVICE)
-        
-
+        print('Init Mask dim {}'.format(self.mask.shape))
 
     def params_initialization(self, init_weights, method='k-means'):
         """ Initialization of GMM parameters using k-means algorithm. """
