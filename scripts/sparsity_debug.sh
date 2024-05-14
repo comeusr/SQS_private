e00b60ae8076080e4059f7ecc4e41904aeba6ea0
Ziyi Wang <wang4538@purdue.edu>
2024-05-14 16:20:48 -0400

    sparsity

--- scripts/sparsity_debug.sh
+++ scripts/sparsity_debug.sh
@@ -17,7 +17,7 @@ INIT_METHOD='k-means'
 INIT_SPARSITY=0.1
 FINAL_SPARSITY=0.7
 PRUNE_END='4ep'
-PRUNE_TEMP=0.002
+PRUNE_TEMP=0.0002
 
 
 sbatch --time=00:30:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
@@ -28,7 +28,7 @@ sbatch --time=00:30:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 
 nvidia-smi
 python ../main.py $DATASET $MODEL $RESUME $GPU $FREEZE --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
-       --lr ${LR} --duration ${EPOCHS} --t_warmup "5ep" --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD}\
+       --lr ${LR} --duration ${EPOCHS} --t_warmup "5ep" --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
        --run_name debug --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} --prune \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
