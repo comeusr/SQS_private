95480f987c18cf68ce2b57fc8f5d5920910d3971
Ziyi Wang <wang4538@purdue.edu>
2024-05-27 21:04:33 -0400

    CIFAR100 scripts

--- scripts/90%sparsity_cifar100.sh
+++ scripts/90%sparsity_cifar100.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
-DATASET="--train-dir /home/wang4538/DGMS-master/CIFAR10/train/ --val-dir /home/wang4538/DGMS-master/CIFAR10/val/ --num-classes 10"
+DATASET="--train-dir /home/wang4538/DGMS-master/CIFAR00/train/ --val-dir /home/wang4538/DGMS-master/CIFAR100/val/ --num-classes 100"
 MODEL="--network resnet18 --mask"
 WD=0
 TEMP=0.001
 K=4
 LR=5e-5
-DATA_NAME="cifar10"
+DATA_NAME="cifar100"
 MODEL_NAME="resnet18"
 EPOCHS="10ep"
 FINAL_LR=0.01
@@ -29,7 +29,7 @@ sbatch --time=01:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 nvidia-smi
 python ../main.py $DATASET $MODEL $RESUME $GPU $FREEZE --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --t_warmup "5ep" --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
-       --run_name K${K}_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD} \
+       --run_name CIFAR100_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD} \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} --prune \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
