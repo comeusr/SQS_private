d27bb2098a5c4a0a5a8d370868feda2ab4d49918
Ziyi Wang <wang4538@purdue.edu>
2024-07-01 16:34:12 -0700

    Tuning

--- utils/algorithm.py
+++ utils/algorithm.py
@@ -175,10 +175,6 @@ class GMM_Pruning(Algorithm):
                 #Prune with mask
                 self.prune_with_mask(state.model)
             
-            # with torch.no_grad():
-            # self.customize_lr_schduler(state, train_step)
-
-            #     self.monitor_scheduler_step(state, logger)
                             
         elif event == Event.AFTER_BACKWARD:
             # Add the gradients of KL divergence to pruning parameters
