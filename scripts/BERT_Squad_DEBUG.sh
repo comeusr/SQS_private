f43e2964062eb07e062e1d06a460b2bd826e85eb
Ziyi Wang <wang4538@purdue.edu>
2024-08-05 04:17:00 -0700

    Debug Quantization

--- scripts/BERT_Squad_DEBUG.sh
+++ scripts/BERT_Squad_DEBUG.sh
@@ -5,7 +5,7 @@ WD=5e-7
 TEMP=0.0005
 K=4
 LR=2e-5
-EPOCHS=5
+EPOCHS=20
 FINAL_LR=0.01
 EVAL_INTERV='1ep'
 SEED=428
@@ -21,14 +21,14 @@ SIGMA=3
 # CIFAR100_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
 
 
-sbatch --time=01:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
+sbatch --time=02:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #!/bin/bash -l
 
 #SBATCH --output /home/wang4538/DGMS-master/out/%j.out
 #SBATCH --error /home/wang4538/DGMS-master/out/%j.out
 
 nvidia-smi
-python ../bert_exp.py $DATASET  --K ${K} --tau ${TEMP} --weight_decay ${WD} --debug \
+python ../bert_exp.py $DATASET  --K ${K} --tau ${TEMP} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
        --run_name nonF_Squad_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_PRTEMP${PRUNE_TEMP}_WD${WD}_SIGMA${SIGMA}  \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --warm_up ${WARM_UP} --sigma ${SIGMA} \
