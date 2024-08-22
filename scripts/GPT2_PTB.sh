6bd243a448176cd8388f765baf0b6368ca635b42
Ziyi Wang <wang4538@purdue.edu>
2024-08-22 02:42:17 -0700

    Add GPT2 experiments on PTB datasets.

--- scripts/GPT2_PTB.sh
+++ scripts/GPT2_PTB.sh
@@ -3,7 +3,7 @@
 DATASET="--dataset_name ptb_text_only"
 WD=5e-7
 TEMP=0.0005
-K=3
+K=2
 LR=2e-5
 EPOCHS=3
 FINAL_LR=0.01
@@ -28,9 +28,9 @@ sbatch --time=04:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #SBATCH --error /home/wang4538/DGMS-master/out/%j.outs
 
 nvidia-smi
-python ../GPT2_exp.py $DATASET  --K ${K} --tau ${TEMP} --weight_decay ${WD} --prune --sample \
+python ../GPT2_exp.py $DATASET  --K ${K} --tau ${TEMP} --weight_decay ${WD} --prune \
        --lr ${LR} --duration ${EPOCHS} --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
-       --run_name GPT2_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_PRTEMP${PRUNE_TEMP}_WD${WD}_SIGMA${SIGMA}  \
+       --run_name NonSample_GPT2_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_PRTEMP${PRUNE_TEMP}_WD${WD}_SIGMA${SIGMA}  \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --prune_start ${WARM_UP} --sigma ${SIGMA} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_GPT2/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
