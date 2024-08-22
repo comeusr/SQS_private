24869ebfde13fd74aa87db5f6d048e1a67335c15
Ziyi Wang <wang4538@purdue.edu>
2024-08-22 01:16:51 -0700

    Add GPT2 experiments on PTB datasets.

--- GPT2_exp.py
+++ GPT2_exp.py
@@ -281,9 +281,9 @@ def main():
             with torch.no_grad():
                 outputs = model(batch["input_ids"], labels=batch["input_ids"])
 
-            print("Eval losses {}".format(outputs.loss.item()))
+            # print("Eval losses {}".format(outputs.loss.item()))
 
-            losses.append(accelerator.gather(outputs.loss))
+            losses.append(accelerator.gather(outputs.loss.item().reshape(1)))
         
         print(losses)
         loss = torch.mean(torch.cat(losses))
