ec55b68861c6c61ceb9c7d15e858550210d7727e
Ziyi Wang <wang4538@purdue.edu>
2024-08-24 21:56:23 -0700

    Add GPT2 experiments on PTB datasets.

--- scripts/GPT2_PTB.sh
+++ scripts/GPT2_PTB.sh
@@ -30,7 +30,7 @@ sbatch --time=04:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 nvidia-smi
 python ../GPT2_exp.py $DATASET  --K ${K} --tau ${TEMP} --weight_decay ${WD} --prune --sample --debug \
        --lr ${LR} --duration ${EPOCHS} --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
-       --run_name NonSample_GPT2_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_PRTEMP${PRUNE_TEMP}_WD${WD}_SIGMA${SIGMA}  \
+       --run_name GPT2_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_LR${LR}_PRTEMP${PRUNE_TEMP}_WD${WD}_SIGMA${SIGMA}  \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --prune_start ${WARM_UP} --sigma ${SIGMA} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_GPT2/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
