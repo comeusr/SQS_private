6673d7035d15401c831ad1b56510aed39c05c765
Ziyi Wang <wang4538@purdue.edu>
2024-09-20 20:48:38 -0400

    Change Pretrain Model.

--- scripts/70%sparsity_cifar10.sh
+++ scripts/70%sparsity_cifar10.sh
@@ -28,7 +28,7 @@ sbatch --time=01:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #SBATCH --error /home/wang4538/DGMS-master/out/%j.out
 
 nvidia-smi
-python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} --sample \
+python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --t_warmup WARM_UP --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
        --run_name sample_${DATA_NAME}_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_prtemp${PRUNE_TEMP}_LR${LR}_F${FINAL_LR}_WD${WD} \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --prune_init_lr ${PRUNE_INIT_LR} \
