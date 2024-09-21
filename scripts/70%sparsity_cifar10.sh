6aee3e15fd3e37d9ab07c2bb8671e96fbf2b28cc
Ziyi Wang <wang4538@purdue.edu>
2024-09-21 06:40:08 -0400

    Change Pretrain Model.

--- scripts/70%sparsity_cifar10.sh
+++ scripts/70%sparsity_cifar10.sh
@@ -19,7 +19,7 @@ FINAL_SPARSITY=0.40
 PRUNE_END='15ep'
 PRUNE_TEMP=0.03
 WARM_UP='1ep'
-PRUNE_INIT_LR=0.015
+PRUNE_INIT_LR=0.02
 
 sbatch --time=01:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #!/bin/bash -l
