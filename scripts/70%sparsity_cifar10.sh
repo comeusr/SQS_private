1700b9b0c909f74b7442d477a5e885ceb36c6724
Ziyi Wang <wang4538@purdue.edu>
2024-09-21 09:13:39 -0400

    Change Pretrain Model.

--- scripts/70%sparsity_cifar10.sh
+++ scripts/70%sparsity_cifar10.sh
@@ -3,7 +3,7 @@
 DATASET="--train-dir /home/wang4538/DGMS-master/CIFAR10/train/ --val-dir /home/wang4538/DGMS-master/CIFAR10/val/ --num-classes 10"
 MODEL="--network resnet32 --mask"
 WD=1e-4
-TEMP=0.001
+TEMP=0.002
 K=4
 LR=5e-5
 DATA_NAME="cifar10"
@@ -17,7 +17,7 @@ INIT_METHOD='k-means'
 INIT_SPARSITY=0.0
 FINAL_SPARSITY=0.50
 PRUNE_END='15ep'
-PRUNE_TEMP=0.025
+PRUNE_TEMP=0.02
 WARM_UP='1ep'
 PRUNE_INIT_LR=0.015
 
