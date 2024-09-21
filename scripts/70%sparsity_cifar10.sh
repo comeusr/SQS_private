11ae2e62b7ab12543abcd57d544807ef607c5d46
Ziyi Wang <wang4538@purdue.edu>
2024-09-21 01:24:31 -0400

    Change Pretrain Model.

--- scripts/70%sparsity_cifar10.sh
+++ scripts/70%sparsity_cifar10.sh
@@ -28,9 +28,9 @@ sbatch --time=01:00:00 --nodes=1 --gpus-per-node=1 --mem-per-gpu=40g <<EOT
 #SBATCH --error /home/wang4538/DGMS-master/out/%j.out
 
 nvidia-smi
-python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
+python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} --sample \
        --lr ${LR} --duration ${EPOCHS} --t_warmup WARM_UP --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
-       --run_name nonsample_${DATA_NAME}_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_prtemp${PRUNE_TEMP}_LR${LR}_F${FINAL_LR}_WD${WD} \
+       --run_name sample_${DATA_NAME}_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_prtemp${PRUNE_TEMP}_LR${LR}_F${FINAL_LR}_WD${WD} \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --prune_init_lr ${PRUNE_INIT_LR} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} --prune \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
