fc04f094a2ed100408392163ad34c086b01d5836
Ziyi Wang <wang4538@purdue.edu>
2024-09-23 18:16:59 -0400

    Enable Beyasian Average.

--- scripts/70%sparsity_cifar100.sh
+++ scripts/70%sparsity_cifar100.sh
@@ -32,7 +32,7 @@ nvidia-smi
 python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --t_warmup "5ep" --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
        --run_name sample_${DATA_NAME}_${MODEL_NAME}_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_prtemp${PRUNE_TEMP}_LR${LR}_prLR${PRUNE_INIT_LR}_F${FINAL_LR}_WD${WD}_${AVERAGE_NUM} \
-       --sample --average --average_num ${AVERAGE_NUM} \ 
+       --sample --average --average_num ${AVERAGE_NUM} \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --warm_up ${WARM_UP}\
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} --prune \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
