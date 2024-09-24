fb62f4694fb31892c58a3ff587f68ac4a444257b
Ziyi Wang <wang4538@purdue.edu>
2024-09-23 21:58:44 -0400

    Enable Beyasian Average.

--- scripts/70%sparsity_cifar100.sh
+++ scripts/70%sparsity_cifar100.sh
@@ -2,14 +2,14 @@
 
 DATASET="--train-dir /home/wang4538/DGMS-master/CIFAR100/train/ --val-dir /home/wang4538/DGMS-master/CIFAR100/val/ --num-classes 100"
 MODEL="--network resnet18 --mask"
-WD=0
+WD=5e-6
 TEMP=0.001
 K=16
 LR=5e-5
 DATA_NAME="cifar100"
 MODEL_NAME="resnet18"
 EPOCHS="11ep"
-FINAL_LR=0.01
+FINAL_LR=0.007
 EVAL_INTERV='1ep'
 SEED=428
 FREEZE="--freeze_weight"
@@ -33,7 +33,7 @@ nvidia-smi
 python ../main.py $DATASET $MODEL $RESUME $GPU --K ${K} --tau ${TEMP} --dataset ${DATA_NAME} --weight_decay ${WD} \
        --lr ${LR} --duration ${EPOCHS} --t_warmup "5ep" --alpha_f ${FINAL_LR} --seed ${SEED} --init_method ${INIT_METHOD} \
        --run_name sample_${DATA_NAME}_${MODEL_NAME}_K${K}_KL_SPAS${FINAL_SPARSITY}_temp${TEMP}_prtemp${PRUNE_TEMP}_LR${LR}_prLR${PRUNE_INIT_LR}_F${FINAL_LR}_WD${WD}_${AVERAGE_NUM} \
-       --sample --average_num ${AVERAGE_NUM} \
+       --sample --average --average_num ${AVERAGE_NUM} \
        --autoresume --eval_interval ${EVAL_INTERV} --prune_scale ${PRUNE_TEMP} --warm_up ${WARM_UP} \
        --init_sparsity ${INIT_SPARSITY} --final_sparsity ${FINAL_SPARSITY} --prune_end ${PRUNE_END} --prune  --prune_init_lr ${PRUNE_INIT_LR} \
        --save_folder /scratch/gilbreth/wang4538/DGMS/Run/${INIT_METHOD}${DATA_NAME}_${MODEL_NAME}/K${K}_temp${TEMP}_LR${LR}_F${FINAL_LR}_WD${WD}
